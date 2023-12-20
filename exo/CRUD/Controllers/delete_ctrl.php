11020e91f22ede66d5e6974d834350b9a036ac6c
IlyesX57 <baylan.ilyes2.0@gmail.com>
2023-12-20 16:12:50 +0100

    Maj

---
+++ exo/CRUD/Controllers/delete_ctrl.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../db.php');
+
+
+if (isset($_POST) && !empty($_POST)) {
+    $delete = $BDD->prepare('DELETE FROM livre WHERE id=?');
+    $delete->execute(array(
+        $_POST['delete']
+    ));
+    header('Location: ../index.php');
+} else {
+    header('Location: ../index.php');
+}
